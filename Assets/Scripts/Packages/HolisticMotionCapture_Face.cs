ebcd19b3ea080e394a1cc35e8ce2f0dcb2bb3bb4
IKEP <34697515+creativeIKEP@users.noreply.github.com>
2021-11-19 22:35:14 +0900

    mouth

--- Assets/Scripts/Packages/HolisticMotionCapture_Face.cs
+++ Assets/Scripts/Packages/HolisticMotionCapture_Face.cs
@@ -12,6 +12,8 @@ partial class HolisticMotionCapture
     }
 
     void FaceRender(ComputeBuffer faceVertexBuffer, ComputeBuffer leftEyeVertexBuffer, ComputeBuffer rightEyeVertexBuffer, bool isSeparateEyeBlink){
+        var faceLandmarks = new Vector4[faceVertexCount];
+        faceVertexBuffer.GetData(faceLandmarks);
         var leftEyeLandmarks = new Vector4[eyeVertexCount];
         leftEyeVertexBuffer.GetData(leftEyeLandmarks);
         var rightEyeLandmarks = new Vector4[eyeVertexCount];
@@ -19,6 +21,7 @@ partial class HolisticMotionCapture
 
         BlinkRender(leftEyeLandmarks, rightEyeLandmarks, isSeparateEyeBlink);
         PupilRender(leftEyeLandmarks, rightEyeLandmarks);
+        MouthRender(faceLandmarks);
     }
 
     void BlinkRender(Vector4[] leftEyeLandmarks, Vector4[] rightEyeLandmarks, bool isSeparateEyeBlink){
@@ -108,5 +111,48 @@ partial class HolisticMotionCapture
         if(float.IsInfinity(ratioX) || float.IsNaN(ratioX)) ratioX = 0;
         if(float.IsInfinity(ratioY) || float.IsNaN(ratioY)) ratioY = 0;
         return new Vector2(ratioX, ratioY);
+    }
+
+    void MouthRender(Vector4[] faceLandmarks){
+        var eyeInnerCornerL = faceLandmarks[133];
+        var eyeInnerCornerR = faceLandmarks[362];
+        var eyeOuterCornerL = faceLandmarks[130];
+        var eyeOuterCornerR = faceLandmarks[263];
+
+        var eyeInnerDistance = Vector3.Distance(eyeInnerCornerL, eyeInnerCornerR);
+        var eyeOuterDistance = Vector3.Distance(eyeOuterCornerL, eyeOuterCornerR);
+
+        var upperInnerLip = faceLandmarks[13];
+        var lowerInnerLip = faceLandmarks[14];
+        var mouthCornerLeft = faceLandmarks[61];
+        var mouthCornerRight = faceLandmarks[291];
+
+        var mouthOpen = Vector3.Distance(upperInnerLip, lowerInnerLip);
+        var mouthWidth = Vector3.Distance(mouthCornerLeft, mouthCornerRight);
+
+        var ratioY = mouthOpen / eyeInnerDistance;
+        var ratioX = mouthWidth / eyeOuterDistance;
+
+        ratioY = (ratioY - 0.15f) / (0.7f - 0.15f);
+        ratioX = (ratioX - 0.45f) / (0.9f - 0.45f);
+        ratioX = (ratioX - 0.3f) * 2f;
+        
+        var mouthX = ratioX;
+        var mouthY = (mouthOpen / eyeInnerDistance - 0.17f) / (0.5f - 0.17f);
+
+        var ratioI = Mathf.Clamp(mouthX * 2 * ((mouthY - 0.2f) / 0.5f), 0, 1);
+        var ratioA = mouthY * 0.4f + mouthY * (1 - ratioI) * 0.6f;
+        var ratioU = mouthY * ((1-ratioI) / 0.3f) * 0.01f;
+        var ratioE = ((ratioU - 0.2f) / 0.8f) * (1 - ratioI) * 0.3f;
+        var ratioO = (1 - ratioI) * 0.4f * ((mouthY - 0.3f) / 0.7f);
+
+        proxy.SetValues(new Dictionary<BlendShapeKey, float>
+        {
+            {BlendShapeKey.CreateFromPreset(BlendShapePreset.A), Mathf.Clamp(ratioA, 0, 1)},
+            {BlendShapeKey.CreateFromPreset(BlendShapePreset.I), Mathf.Clamp(ratioI, 0, 1)},
+            {BlendShapeKey.CreateFromPreset(BlendShapePreset.U), Mathf.Clamp(ratioU, 0, 1)},
+            {BlendShapeKey.CreateFromPreset(BlendShapePreset.E), Mathf.Clamp(ratioE, 0, 1)},
+            {BlendShapeKey.CreateFromPreset(BlendShapePreset.O), Mathf.Clamp(ratioO, 0, 1)},
+        });
     }
 }

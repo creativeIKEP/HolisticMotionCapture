709c1d3702e797c64e2cfea24fed8bdeeb78955f
IKEP <34697515+creativeIKEP@users.noreply.github.com>
2022-12-30 01:15:59 +0900

    Revert "pose reset"
    This reverts commit c28242fae29af268e975f4ddd85dcff22303f70c.

--- Assets/Scripts/Packages/HolisticMotionCapture.cs
+++ Assets/Scripts/Packages/HolisticMotionCapture.cs
@@ -14,13 +14,12 @@ public partial class HolisticMotionCapture : System.IDisposable
     }
     
     Animator avatar;
-    HolisticMocapType mocapType;
 
     public HolisticMotionCapture(Animator avatarAnimator, BlazePoseModel blazePoseModel = BlazePoseModel.full){
         avatar = avatarAnimator;
         holisticPipeline = new HolisticPipeline(blazePoseModel);
-        PoseInit();
         HandInit();
+        PoseInit();
         FaceInit();
     }
 
@@ -42,21 +41,15 @@ public partial class HolisticMotionCapture : System.IDisposable
         float poseDetectionIouThreshold = 0.3f)
     {
         holisticPipeline.ProcessImage(inputTexture, (HolisticInferenceType)mocapType, blazePoseModel, poseDetectionThreshold, poseDetectionIouThreshold);
-
-        if(this.mocapType != mocapType){
-            ResetAvatar();
-            this.mocapType = mocapType;
-        }
-
         PoseRender(mocapType, poseScoreThreshold, isUpperBodyOnly, lerpPercentage);
         HandRender(mocapType, true, handScoreThreshold, lerpPercentage);
         HandRender(mocapType, false, handScoreThreshold, lerpPercentage);
         FaceRender(mocapType, faceScoreThreshold, isSeparateEyeBlink);
     }
 
-    void ResetAvatar() {
-        ResetPose(1);
-        ResetHand(true, 1);
-        ResetHand(false, 1);
+    public void ResetAvatar(float lerpPercentage = 0.3f) {
+        ResetPose(lerpPercentage);
+        ResetHand(true, lerpPercentage);
+        ResetHand(false, lerpPercentage);
     }
 }

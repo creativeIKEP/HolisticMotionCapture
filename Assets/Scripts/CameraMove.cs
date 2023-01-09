318a30c03c353ea6911acb672a5534fb8217abaf
IKEP <34697515+creativeIKEP@users.noreply.github.com>
2023-01-09 19:51:08 +0900

    ui hide when click

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -7,15 +7,25 @@ public class CameraMove : MonoBehaviour
 {
     [SerializeField] float CameraMoveSpeed;
     [SerializeField] float CameraZMoveSpeed;
+    [SerializeField] CanvasGroup controlCanvas;
+    [SerializeField] Camera PredictionCamera;
+
+    bool isDragging = false;
+    bool isUiShow = true;
 
     public void OnClick(BaseEventData data)
     {
-        Debug.Log("click");
+        var pointerData = data as PointerEventData;
+        if (pointerData.dragging) return;
+        isUiShow = !isUiShow;
+        controlCanvas.alpha = isUiShow ? 1 : 0;
+        controlCanvas.interactable = isUiShow;
+        controlCanvas.blocksRaycasts = isUiShow;
+        PredictionCamera.enabled = isUiShow;
     }
 
     public void OnDrag(BaseEventData data)
     {
-        Debug.Log("drag");
         var pointerData = data as PointerEventData;
         var deltaPosition = pointerData.delta * CameraMoveSpeed * Time.deltaTime;
         Camera.main.transform.localPosition += new Vector3(deltaPosition.x, -deltaPosition.y, 0);
@@ -23,7 +33,6 @@ public class CameraMove : MonoBehaviour
 
     public void OnScroll(BaseEventData data)
     {
-        Debug.Log("scroll");
         var pointerData = data as PointerEventData;
         var deltaPosition = pointerData.scrollDelta.y * CameraZMoveSpeed * Time.deltaTime;
         Camera.main.transform.localPosition += new Vector3(0, 0, deltaPosition);
